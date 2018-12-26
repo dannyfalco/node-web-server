30c57c63022ab9c1e12ab2a0b409c88780ce69cf
Falco Danny (CORP) <danny.falco@adp.com>
2018-12-26 12:22:45 -0500

    Add project page

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 //route /bad - send back JSON with error message property - error handling request
 app.get('/bad', (req, res) => {
   res.send({
